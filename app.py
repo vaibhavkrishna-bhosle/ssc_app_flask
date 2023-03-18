7157e02ae2b77a442197b229a93f45643b628b11
Vaibhavkrishna Bhosle <vaibhavkrishna.bhosle@gmail.com>
2023-03-19 01:50:28 +0530

    oracle test
    db test

--- app.py
+++ app.py
@@ -14,21 +14,6 @@ def get_user_logins():
     cursor.close()
     return result
 
-app = Flask(__name__)
-@app.route('/')
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0)
-                   ]
+user_logins = get_user_logins()
 
-    return render_template('index.html', times=dummy_times)
-
-
-@app.route('/user_logins', methods=['GET'])
-def user_logins():
-    user_logins = get_user_logins()
-    return jsonify(user_logins)
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+print(user_logins)
